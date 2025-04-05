3b72554ec0583be9e3c79be99fdaa3b249680757
Nikhil Chakravarthy Mallela <nikhilchakravarthy.mallela@gmail.com>
2025-04-05 15:21:25 +0530

    Update HomePage to use improved components

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PageLayout from '@/components/layout/PageLayout';
-import Hero from '@/components/home/Hero';
+import HeroImproved from '@/components/home/HeroImproved';
 import ProblemStatement from '@/components/home/ProblemStatement';
 import DomainApproach from '@/components/home/DomainApproach';
 import KeyFeatures from '@/components/home/KeyFeatures';
 import MissionVision from '@/components/home/MissionVision';
-import ContactUs from '@/components/home/ContactUs';
+import ContactSectionImproved from '@/components/home/ContactSectionImproved';
 import CallToAction from '@/components/home/CallToAction';
 import Testimonials from '@/components/home/Testimonials';
 import CohortAdvantageCarousel from '@/components/home/CohortAdvantageCarousel';
@@ -13,14 +13,14 @@ import CohortAdvantageCarousel from '@/components/home/CohortAdvantageCarousel';
 export default function HomePage() {
   return (
     <PageLayout>
-      <Hero />
+      <HeroImproved />
       <ProblemStatement />
       <DomainApproach />
       <CohortAdvantageCarousel />
       <KeyFeatures />
       <MissionVision />
       <Testimonials />
-      <ContactUs />
+      <ContactSectionImproved />
       <CallToAction />
     </PageLayout>
   );
